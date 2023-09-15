4450902183859e8459183c02c073093c08b2920a
Yunus Gungor <yunusgnr@gmail.com>
2023-09-15 08:26:51 +0200

    name fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="packet-yunusgnr",
     version="0.0.1",
     author="Yunus Gungor",
     author_email="yunusgnr@gmail.com",
